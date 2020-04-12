a3982f5f6301cfda28c261a4d4c0d060e3dff396
Elijah Guerrero <elijahguerrero97@gmail.com>
2020-04-11 19:49:55 -0500

    init

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,6 @@
+INSERT INTO noteful_folders (id, foldername)
+VALUES
+(1, 'Black'),
+(2, 'Blue'),
+(3, 'Pink'),
+(4, 'Purple');
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,6 @@
+INSERT INTO noteful_notes (id, name, folderId, content)
+VALUES
+(1, 'High up in the sky', 1, 'Let them play the music on one fine day'),
+(2, 'Note on music', 1, 'Music so cheerful. Dance all the way.'),
+(3, 'Pitch the idea', 3, 'Good idea gets better when shared.'),
+(4, 'Super note', 4, 'Buy some food.');
